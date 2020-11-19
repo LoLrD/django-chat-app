6168627ef3ca90b31c4d20f884c237422b496b1a
LoLrD <lolrdmen@gmail.com>
2020-11-19 10:43:37 +0600

    add python-dotenv package

--- chatApp/wsgi.py
+++ chatApp/wsgi.py
@@ -10,6 +10,11 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+
+
+env_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+load_dotenv(dotenv_path=env_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatApp.settings')
 
